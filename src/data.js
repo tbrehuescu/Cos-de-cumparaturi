a0ca725501dbdc9647dbaa7bc8d7cfbf15c32b97
teobrehuescu <brehuescu.teodora@gmail.com>
2019-05-02 13:11:30 +0300

    added initial sources

---
+++ src/data.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const data = {
+    items: [
+        {
+            name: "Supa",
+            price: 10.45,
+            image: "img/supa.jpg"
+        },
+        {
+            name: "Friptura",
+            price: 11.35,
+            image: "img/friptura.jpg"
+        },
+
+        {
+            name: "Somon",
+            price: 45.85,
+            image: "img/somon.jpg"
+        },
+        {
+            name: "Prajtura cu banane",
+            price: 12.85,
+            image: "img/banane.jpg"
+        },
+        {
+            name: "Briosa",
+            price: 9.55,
+            image: "img/briosa.jpg"
+        },
+        {
+            name: "Tort de fructe",
+            price: 32.55,
+            image: "img/tort.jpg"
+        }                
+    ]
+};
