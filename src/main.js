af43736fa34ebfa8ee5bd15a372ef3f523a2bfad
teobrehuescu <brehuescu.teodora@gmail.com>
2019-05-02 16:55:27 +0300

    added task 1.3

---
+++ src/main.js
@@ -0,0 +1,12 @@
+var productTemplate = document.getElementById("product-template");
+var products = document.getElementById("data");
+
+// task 1.3
+data.items.forEach(function(item) {
+    var clone = productTemplate.content.cloneNode(true);
+    clone.querySelector(".produs").querySelector(".name").innerHTML = item.name;
+    clone.querySelector(".produs").querySelector(".price").innerHTML = item.price;
+    clone.querySelector(".produs").querySelector(".image").src = item.image;
+
+    products.appendChild(clone);
+});
